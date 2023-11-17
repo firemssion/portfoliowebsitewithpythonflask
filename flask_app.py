118b851cef8634a7c90d4662fdecd3639cdd0413
firemssion <spyder2express@hotmail.com>
2023-11-17 13:00:18 +0000

    Transfer from username firemission3 to jatsea

---
+++ flask_app.py
@@ -0,0 +1,107 @@
+from flask import Flask, redirect, render_template, request, url_for
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import login_user, current_user, LoginManager, UserMixin, login_required, logout_user
+from werkzeug.security import check_password_hash
+import pytz
+from datetime import datetime
+import os
+
+app = Flask(__name__)
+app.config["DEBUG"] = True
+
+app.config["SQLALCHEMY_DATABASE_URI"] = "mysql+mysqlconnector://{username}:{password}@{hostname}/{databasename}".format(
+    username=os.getenv("DB_USERNAME"),
+    password=os.getenv("DB_PASSWORD"),
+    hostname=os.getenv("DB_HOSTNAME"),
+    databasename="jatsea$comments",
+)
+
+app.config["SQLALCHEMY_POOL_RECYCLE"] = 299
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+db = SQLAlchemy(app)
+
+
+app.secret_key = os.getenv("SECRET_KEY")
+login_manager = LoginManager()
+login_manager.init_app(app)
+
+class User(UserMixin, db.Model):
+
+    __tablename__= "users"
+
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(128))
+    password_hash = db.Column(db.String(128))
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
+
+    def get_id(self):
+        return self.username
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.filter_by(username=user_id).first()
+
+singapore = pytz.timezone('Asia/Singapore')
+
+class Comment(db.Model):
+
+    __tablename__ = "comments"
+
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.String(4096))
+    posted = db.Column(db.DateTime, default=datetime.now(singapore))
+    commenter_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=True)
+    commenter = db.relationship('User', foreign_keys=commenter_id)
+
+@app.route('/')
+def about():
+    return render_template('about.html')
+
+@app.route("/skills/")
+def skills():
+    return render_template('skills.html')
+
+@app.route("/projects/")
+def projects():
+    return render_template('projects.html')
+
+@app.route("/awards/")
+def awards():
+    return render_template('awards.html')
+
+@app.route("/comment/", methods=["GET", "POST"])
+def com():
+    if request.method == "GET":
+        return render_template("comment.html", comments=Comment.query.order_by(Comment.posted.desc()).all())
+
+    if not current_user.is_authenticated:
+        return redirect(url_for('com'))
+
+    comment = Comment(content=request.form["contents"], commenter=current_user)
+    db.session.add(comment)
+    db.session.commit()
+    return redirect(url_for('com'))
+
+@app.route("/login/", methods=["GET", "POST"])
+def login():
+    if request.method == "GET":
+        return render_template("login_page.html", error=False)
+
+    user = load_user(request.form["username"])
+    if user is None:
+        return render_template("login_page.html", error=True)
+
+    if not user.check_password(request.form["password"]):
+        return render_template("login_page.html", error=True)
+
+    login_user(user)
+    return redirect(url_for('com'))
+
+@app.route("/logout/")
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('login'))
